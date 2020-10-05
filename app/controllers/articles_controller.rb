9d54819301e7334a369cad88d7b6f93422a3e6f8
Shangni Hu <shangnihu@MacBook-Pro.local>
2020-10-04 21:16:14 -0400

    Finished create new articles route and action, added validation messages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new 
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+         if @article.save
+            # Where do we want this article to go when we save it?
+            flash[:notice] = "Article successfully created."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
 end
